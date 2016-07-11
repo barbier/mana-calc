232faea1555502ffe91c2c23257077cea6835e7e
Alan Gomes <alanhr2@gmail.com>
2016-07-10 21:48:58 -0300

    get all input of type number

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,51 @@
 var gulp = require('gulp'),
-  stylus = require('gulp-stylus'),
-  concat = require('gulp-concat'),
-  connect = require('gulp-connect'),
-  csso = require('gulp-csso'),
-  uglify = require('gulp-uglify'),
-  babel = require('gulp-babel');
+    stylus = require('gulp-stylus'),
+    concat = require('gulp-concat'),
+    connect = require('gulp-connect'),
+    csso = require('gulp-csso'),
+    uglify = require('gulp-uglify'),
+    babel = require('gulp-babel');
 
-gulp.task('html', function () {
-  gulp.src('./*.html')
-    .pipe(connect.reload());
+gulp.task('html', function() {
+    gulp.src('./*.html')
+        .pipe(connect.reload());
 });
 
-gulp.task('styles', function () {
-  gulp.src([
-      './bower_components/pure/pure.css',
-      './style/stylus/main.styl'
-    ])
-    .pipe(stylus({
-      "include css": true
-    }))
-    .pipe(concat('main.css'))
-    .pipe(csso())
-    .pipe(gulp.dest('./style/css'))
-    .pipe(connect.reload());
+gulp.task('styles', function() {
+    gulp.src([
+            './bower_components/pure/pure.css',
+            './style/stylus/main.styl'
+        ])
+        .pipe(stylus({
+            "include css": true
+        }))
+        .pipe(concat('main.css'))
+        .pipe(csso())
+        .pipe(gulp.dest('./style/css'))
+        .pipe(connect.reload());
 });
 
-gulp.task('scripts', function () {
-  gulp.src('./script/app.js')
-    .pipe(babel({presets: ['es2015']}))
-    .pipe(concat('app.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('./script'));
+gulp.task('scripts', function() {
+    gulp.src('./script/app.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(concat('app.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./script'));
 });
 
-gulp.task('watch', function () {
-  gulp.watch('style/**/*.styl', ['styles']);
-  gulp.watch('./*.html', ['html'])
-  gulp.watch('script/app.js', ['scripts']);
+gulp.task('watch', function() {
+    gulp.watch('style/**/*.styl', ['styles']);
+    gulp.watch('./*.html', ['html'])
+    gulp.watch('script/app.js', ['scripts']);
 });
 
-gulp.task('connect', function () {
-  connect.server({
-    livereload: true
-  });
+gulp.task('connect', function() {
+    connect.server({
+        livereload: true,
+        port: 8888
+    });
 });
 
 gulp.task('default', ['styles', 'scripts', 'watch', 'connect']);
